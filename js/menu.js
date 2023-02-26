dd30d63cea07709355a683c244ab94c82d388ce3
YuraWin <63977798+YuraWin@users.noreply.github.com>
2023-02-26 14:12:21 +0200

    finish

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,16 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const links = document.querySelector('[data-links]');
 
   menuBtnRef.addEventListener('click', () => {
     menuBtnRef.classList.toggle('menu-button--cross');
 
+    mobileMenuRef.classList.toggle('is-open');
+  });
+
+  links.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('menu-button--cross');
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
